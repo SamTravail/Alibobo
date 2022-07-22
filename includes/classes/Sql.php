f0d26d9fa0d71b5044bf5a2cfa5cc719c99e2342
Samuel <slanguet@protonmail.com>
2022-07-22 12:41:27 +0200

    Update Sql.php
    ajout pdo

--- includes/classes/Sql.php
+++ includes/classes/Sql.php
@@ -3,9 +3,25 @@
 class Sql
 {
     private string $serverName = "localhost";
-    private  string $userName = "root";
-    private  string $userPassword = "";
-    private  string $database = "alibobo";
-    private  object $connexion;
-    
+    private string $userName = "root";
+    private string $userPassword = "";
+    private string $database = "alibobo";
+    private object $connexion;
+
+    public function __construct()
+    {
+        try {
+            $this->connexion = new PDO("mysql:host=$this->serverName;dbname=$this->database", $this->userName, $this->userPassword);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+
+    public function __destruct()
+    {
+        if (isset($this->connexion))
+            $this->connexion = null;
+    }
 }
