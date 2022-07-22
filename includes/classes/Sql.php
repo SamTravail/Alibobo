8523b184eaf64457a3bc8619d1a0697978338ac9
Samuel <slanguet@protonmail.com>
2022-07-22 12:23:11 +0200

    creation classes
    creation classes

---
+++ includes/classes/Sql.php
@@ -0,0 +1,11 @@
+<?php
+
+class Sql
+{
+    private string $serverName = "localhost";
+    private  string $userName = "root";
+    private  string $userPassword = "";
+    private  string $database = "alibobo";
+    private  object $connexion;
+    
+}
